bc69d03a36c5f0e2735b7a185e1ee107457aa9a3
Ryan Wong <ryanwong935@gmail.com>
2022-05-25 23:03:03 -0400

    test

--- bot.py
+++ bot.py
@@ -188,7 +188,7 @@ async def score(ctx, team):
     soup = BeautifulSoup(req.text, 'html.parser')
     index = 0
     num_teams = len(soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL"))
-    team = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")
+    teamtest = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")
     #"TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp" scores 
     # for i in range(num_teams):
     #     if team[index].get_text() == str(team):
