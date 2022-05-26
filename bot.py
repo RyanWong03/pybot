eaea658d555fca3aeb19a17ad224d170725987c8
Ryan Wong <ryanwong935@gmail.com>
2022-05-25 21:15:43 -0400

    test

--- bot.py
+++ bot.py
@@ -189,14 +189,12 @@ async def score(ctx, team):
     index = 0
     num_teams = len(soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL"))
     team = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")
-    for i in team:
+    #"TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp" scores 
+    for i in num_teams:
         index += 1
         if team[index].get_text() == 'Yankees':
-            test = str(index)
             await ctx.send('Yankees: ' + str(index))    
     
-    print('index: ' + str(index))
-    await ctx.send(test)
 
     if num_teams == 30:
         try:
