a66c3edf2257c6b64fd140c1d043a450a4287479
Ryan Wong <ryanwong935@gmail.com>
2022-06-02 19:19:02 -0400

    test

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def on_ready():
     await client.change_presence(status = discord.Status.idle, activity = discord.Activity(type = discord.ActivityType.playing, name = "$help"))
     await discordUser.send('Bot Online')
     print('Bot is ready.')
-    await general.send('L')
+    #await general.send('L')
     away_score = 0
     away_team_score = 0
     home_score = 0
@@ -278,6 +278,8 @@ async def on_ready():
             await channel.send("Yankees game has started.")
         elif game_stat == 'BOT 5':
             await channel.send('bottom 5 inning')
+        
+        await channel.send('lol')
         if len(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")) >= team_index:
             
             away_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index - 1].get_text())
