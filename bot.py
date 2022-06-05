17ef13fafe05ed94c82e752b1c3c050552ce562e
Ryan Wong <ryanwong935@gmail.com>
2022-06-05 17:26:01 -0400

    Test

--- bot.py
+++ bot.py
@@ -520,35 +520,35 @@ class Bot(discord.Client):
         soup_lineup = BeautifulSoup(r.text, 'lxml') 
         lineup_list = []
         pitchers = []
-        while var < 1:
-            now = datetime.datetime.now()
-            if now.minute == '535353' or now.minute == 534545:
-                for item in soup_lineup.select("[data-league='NL']:-soup-contains('Mets') .player > a.player-link"):
-                    if item.get('data-razz') == '':
-                        player_name = 'Unknown Player'
-                        lineup_list.append(player_name)
-                    else:
-                        player_name = item.get('data-razz').split("/")[-2].replace("+"," ")
-                        lineup_list.append(player_name)
-                visitors = 'team'
-                pitchers.append(lineup_list[0])
-                pitchers.append(lineup_list[1])
+        # while var < 1:
+        #     now = datetime.datetime.now()
+        #     if now.minute == '535353' or now.minute == 534545:
+        #         for item in soup_lineup.select("[data-league='NL']:-soup-contains('Mets') .player > a.player-link"):
+        #             if item.get('data-razz') == '':
+        #                 player_name = 'Unknown Player'
+        #                 lineup_list.append(player_name)
+        #             else:
+        #                 player_name = item.get('data-razz').split("/")[-2].replace("+"," ")
+        #                 lineup_list.append(player_name)
+        #         visitors = 'team'
+        #         pitchers.append(lineup_list[0])
+        #         pitchers.append(lineup_list[1])
                 
-                await channel.send('Starting Pitchers:\n' + str(visitors) + ': ' + pitchers[1] + '\nYankees: ' + pitchers[0])
+        #         await channel.send('Starting Pitchers:\n' + str(visitors) + ': ' + pitchers[1] + '\nYankees: ' + pitchers[0])
 
-                lineup_list.pop(0)
-                lineup_list.pop(0)
-                n = 9
-                home_list = lineup_list[n:]
-                away_list = lineup_list[:-n]
+        #         lineup_list.pop(0)
+        #         lineup_list.pop(0)
+        #         n = 9
+        #         home_list = lineup_list[n:]
+        #         away_list = lineup_list[:-n]
 
-                away_lineup = """```1: """ + away_list[0] + """\n2: """ + away_list[1] + """\n3: """ + away_list[2] + """\n4: """ + away_list[3] + """\n5: """ + away_list[4] + """\n6: """ + away_list[5] + """\n7: """ + away_list[6] + """\n8: """ + away_list[7] + """\n9: """ + away_list[8] + """```"""
-                await channel.send(away_lineup)
+        #         away_lineup = """```1: """ + away_list[0] + """\n2: """ + away_list[1] + """\n3: """ + away_list[2] + """\n4: """ + away_list[3] + """\n5: """ + away_list[4] + """\n6: """ + away_list[5] + """\n7: """ + away_list[6] + """\n8: """ + away_list[7] + """\n9: """ + away_list[8] + """```"""
+        #         await channel.send(away_lineup)
 
-                home_lineup = """```1: """ + home_list[0] + """\n2: """ + home_list[1] + """\n3: """ + home_list[2] + """\n4: """ + home_list[3] + """\n5: """ + home_list[4] + """\n6: """ + home_list[5] + """\n7: """ + home_list[6] + """\n8: """ + home_list[7] + """\n9: """ + home_list[8] + """```"""
-                await channel.send(home_lineup)
-                #await dm.send(away_lineup)
-                var = 1
+        #         home_lineup = """```1: """ + home_list[0] + """\n2: """ + home_list[1] + """\n3: """ + home_list[2] + """\n4: """ + home_list[3] + """\n5: """ + home_list[4] + """\n6: """ + home_list[5] + """\n7: """ + home_list[6] + """\n8: """ + home_list[7] + """\n9: """ + home_list[8] + """```"""
+        #         await channel.send(home_lineup)
+        #         #await dm.send(away_lineup)
+        #         var = 1
 
 
     async def on_message(self, message):
