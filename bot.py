24ee882a23e2455ed4b2c79f32e7c74ce91c1712
Ryan Wong <ryanwong935@gmail.com>
2022-06-06 16:10:45 -0400

    test

--- bot.py
+++ bot.py
@@ -545,17 +545,18 @@ class Bot(discord.Client):
             now = datetime.datetime.now()
             #hour is 4 hours ahead of EST
             if away_team == True and now.hour >= 2 <= 8:
-                visitors = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index].get_text()
-                home_team = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index + 1].get_text()
-                away_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index].get_text())
-                home_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index + 1].get_text())
-                if away_score != away_team_score:
-                    await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
-                    away_score = away_team_score
+                print(now.hour)
+                # visitors = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index].get_text()
+                # home_team = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index + 1].get_text()
+                # away_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index].get_text())
+                # home_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index + 1].get_text())
+                # if away_score != away_team_score:
+                #     await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
+                #     away_score = away_team_score
                     
-                if home_score != home_team_score:
-                    await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
-                    home_score = home_team_score
+                # if home_score != home_team_score:
+                #     await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
+                #     home_score = home_team_score
                     
             if now.hour == 1 and hr < 1:
                 for item in soup_lineup.select("[data-league='NL']:-soup-contains('Mets') .player > a.player-link"):
