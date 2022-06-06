e8dad33fe44cfcf7e02eb3c835eca46c7e53d440
Ryan Wong <ryanwong935@gmail.com>
2022-06-05 20:13:57 -0400

    test

--- bot.py
+++ bot.py
@@ -541,17 +541,21 @@ class Bot(discord.Client):
         queried_schedule = statsapi.schedule(date = target_date_time.strftime('%Y-%m-%d'), team = int(team_selected['id']))
     
         while var < 1:
-            #now = datetime.datetime.now()
-            if away_team == True:
-                visitors = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index].get_text()
-                home_team = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index + 1].get_text()
-                away_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index].get_text())
-                home_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index + 1].get_text())
-                if away_score != away_team_score:
-                    print('away score diff')
-                    await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
-                    away_score = away_team_score
-                    break
+
+            now = datetime.datetime.now()
+            if now.hour == 9 or now.hour == '9':
+                await channel.send('9PM')
+                break
+            # if away_team == True:
+            #     visitors = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index].get_text()
+            #     home_team = soup.find_all(class_ = "TeamWrappersstyle__DesktopTeamWrapper-sc-uqs6qh-0 iNsMPL")[team_index + 1].get_text()
+            #     away_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index].get_text())
+            #     home_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index + 1].get_text())
+            #     if away_score != away_team_score:
+            #         print('away score diff')
+            #         await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
+            #         away_score = away_team_score
+            #         break
                     
                 # if home_score != home_team_score:
                 #     print('home score diff')
