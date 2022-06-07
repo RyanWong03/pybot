0bb22e473ae28193cdc9a26e0187b85fb584d005
Ryan Wong <ryanwong935@gmail.com>
2022-06-06 23:32:44 -0400

    test

--- bot.py
+++ bot.py
@@ -549,7 +549,7 @@ class Bot(discord.Client):
                 away_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[6].get_text())
                 home_team_score = int(soup.find_all(class_ = "TeamMatchupLayerstyle__ScoreWrapper-sc-3lvmzz-3 cLonxp")[team_index + 1].get_text())
                 if away_score != away_team_score:
-                    print('mets score')
+                    print('mets score1')
                     await self.embedFunctions.scoring_plays_embed(queried_schedule[0], channel)
                     away_score = away_team_score
                     
