f9df6a85a1d03b6a7321c15cecd0e858b1b35d92
Ryan Wong <ryanwong935@gmail.com>
2022-06-08 07:54:20 -0400

    Test

--- bot.py
+++ bot.py
@@ -200,7 +200,7 @@ class EmbedFunctions:
             #box_score_embed.add_field(name = away_box[pitcher]['ip'], inline = True)
 
         #await channel.send(content = 'Box Scores ', embed = box_score_embed)
-        await channel.send(away_team)
+        print(away_team)
 
     async def scheduled_game_embed(self, game, message):
         if type(game) == list:
