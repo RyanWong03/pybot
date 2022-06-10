c1de8cb18ae123121138bd93e35bdd79d8d767a9
Ryan Wong <ryanwong935@gmail.com>
2022-06-10 08:13:16 -0400

    test

--- bot.py
+++ bot.py
@@ -784,8 +784,10 @@ class Bot(discord.Client):
                 yankees_game_time_local = self.testFunctions.get_local_time(yankees_schedule[0]['game_datetime'])
                 yankees_new_hour = yankees_game_time_local - timedelta(hours=4)
                 yankees_new_minute = yankees_game_time_local - timedelta(minutes=5)
-                yankees_away_team_code = yankees_visitors[0]['fileCode'].upper()
-                yankees_home_team_code = yankees_home_team[0]['fileCode'].upper()
+                yankees_away_team_code = self.testFunctions.get_team_no_msg(str(yankees_visitors))[0]['fileCode']
+                print(yankees_away_team_code)
+                break
+                yankees_home_team_code = yankees[0]['fileCode']
                 yankees_home_prob = yankees_schedule[0]['home_probable_pitcher']
                 yankees_away_prob = yankees_schedule[0]['away_probable_pitcher']
                 #yankees_pitchers = await self.embedFunctions.boxscore(int(yankees_game_id))
